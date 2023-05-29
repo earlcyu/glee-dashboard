2d872b4e66863a10ef9f90e5b75ab02de1374665
Earl Yu <earlc.yu@gmail.com>
2023-05-29 14:46:30 +0800

    refine process dim and fact fields functions

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-def remove_first_and_last_characters(string):
-    return string[1:-1]
+def make_snake_case(string):
+    return string.lower().replace(' ', '_')
 
 def extract_character_before_delimeter(string, delimeter):
     position = string.find(delimeter)
@@ -14,17 +14,34 @@ def get_episode_code(season, episode):
     else:
         return f'S0{str(season)}E{episode}'
 
-def process_dim_fields(df: pd.DataFrame, field):
-    df = df.drop_duplicates().reset_index(drop=True)
-    df[field] =  range(1, len(df)+1)
+def process_dim_fields(df: pd.DataFrame, primary_key, col_to_rename=None, col_order=None):
+    # Drop duplicates; Reset index
+    df = df.drop_duplicates(ignore_index=True)
+    # Add a primary key
+    df[primary_key] =  range(1, len(df)+1)
+    # Rename columns
+    if col_to_rename:
+        df = df.rename(columns=col_to_rename)
+    # Re-order columns
+    df = df[col_order]
     return df
 
-def process_fact_fields(df: pd.DataFrame, primary_key, split_col_name, col_to_split, delimeter):
+def process_fact_fields(df: pd.DataFrame, primary_key, split_col_name, col_to_split, delimeter, col_to_rename=None, col_order=None):
+    # Drop duplicates; Reset index
+    df = df.drop_duplicates(ignore_index=True)
+    # Add a primary key    
     df[primary_key] = range(1, len(df)+1)
-    df.columns = df.columns.str.title()
+    # Rename columns
+    if col_to_rename:
+        df = df.rename(columns=col_to_rename)
+    # Split column
     df[split_col_name] = df[col_to_split].str.split(delimeter)
+    # Explode column
     df = df.explode(split_col_name)
     df[split_col_name] = df[split_col_name].str.strip()
+    # Re-order columns
+    if col_order:
+        df = df[col_order]
     return df
 
 
