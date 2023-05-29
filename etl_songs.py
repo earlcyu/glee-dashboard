23426e7f36085dd5c5cd4998ab2ebc0704e061e4
Earl Yu <earlc.yu@gmail.com>
2023-05-29 14:26:41 +0800

    change columns to snake case; condensed processing of dim and fact dataframes

--- etl_songs.py
+++ etl_songs.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd 
-from util import extract_character_before_delimeter, get_episode_code, process_dim_fields, process_fact_fields, export_df_to_csv
+from util import make_snake_case, extract_character_before_delimeter, get_episode_code, process_dim_fields, process_fact_fields, export_df_to_csv
 
 
 
@@ -12,15 +12,18 @@ links = [f'https://en.wikipedia.org/wiki/List_of_songs_in_Glee_(season_{link})' 
 songs = pd.DataFrame()
 for index, link in enumerate(links):
     df = pd.read_html(link)[0]
-    df['Season'] = index + 1
+    df['season'] = index + 1
     songs = pd.concat([songs, df])
 
 
 
 # Part 2: Trasnform data =======================================================
-# Clean Album column
-songs['Album'] = songs['Album'].fillna(songs['Album[nb 1]']).fillna('N/A')
-songs['Album'] = songs['Album'].replace(
+# Make column names into snake case
+songs.columns = map(make_snake_case, songs.columns)
+
+# Clean album column
+songs['album'] = songs['album'].fillna(songs['album[nb_1]']).fillna('N/A')
+songs['album'] = songs['album'].replace(
     {
         '—': 'N/A', 
         'TBA': 'N/A',
@@ -29,8 +32,8 @@ songs['Album'] = songs['Album'].replace(
     }
 )
 
-# Clean Performed by column
-songs['Performed by'] = songs['Performed by'].replace(
+# Clean performed_by column
+songs['performed_by'] = songs['performed_by'].replace(
     to_replace={
         ' and': ',',
         ' with': ',',
@@ -51,96 +54,89 @@ songs['Performed by'] = songs['Performed by'].replace(
     regex=True
 )
 
-# Remove the first and last quotation marks from the Title column
-songs['Title'] = songs['Title'].apply(lambda x: x[1:-1])
+# Remove the first and last quotation marks from the title column
+songs['title'] = songs['title'].str[1:-1]
 
 # Extract episode number
-songs['Episode'] = songs['Episode'].apply(lambda x: extract_character_before_delimeter(x, '.'))
+songs['episode'] = songs['episode'].apply(lambda x: extract_character_before_delimeter(x, '.'))
 
 # Replace Yes/No with True/False in the Single column
-songs['Single'] = songs['Single'].replace({'Yes': True, 'No': False})
+songs['single'] = songs['single'].replace({'Yes': True, 'No': False})
 
 # Add a primary key column
-songs['Song ID'] = range(1, len(songs)+1)
+songs['song_id'] = range(1, len(songs)+1)
 
-# Get the Episode Code by combining Season and Episode
-songs['Episode Code'] = songs[['Season', 'Episode']].apply(lambda x: get_episode_code(*x), axis=1)
+# Get the episode_code by combining Season and episode
+songs['episode_code'] = songs[['season', 'episode']].apply(lambda x: get_episode_code(*x), axis=1)
 
-# Remove Season, Episode, Album
-songs = songs.drop(['Ref.', 'Season', 'Episode'], axis=1)
+# Remove Season, episode, Album
+songs = songs.drop(['ref.', 'season', 'episode'], axis=1)
 
 # Rename columns
-songs = songs.rename(columns={'Title': 'Song Title', 'Single': 'Is Single'})
+songs = songs.rename(columns={'title': 'song_title', 'single': 'is_single'})
 
 
 
 # Part 3: Covers ===============================================================
-covers = songs[['Version covered']]
-
-# Drop duplicates; Add a primary key column
-covers = process_dim_fields(covers, 'Cover ID')
-
-# Rename column
-covers = covers.rename(columns={'Version covered': 'Version Covered Name'})
-
-# Re-order columns
-covers = covers[['Cover ID', 'Version Covered Name']]
+# Drop duplicates; Add a primary key column; Rename; Re-order
+covers = process_dim_fields(
+    df=songs[['version_covered']], 
+    primary_key='cover_id', 
+    col_to_rename={'version_covered': 'cover_name'},
+    col_order=['cover_id', 'cover_name']
+)
 
 
 
 # Part 4: Albums ===============================================================
-albums = songs[['Album']]
-
-# Drop duplicates; Add a primary key column
-albums = process_dim_fields(albums, 'Album ID')
-
-# Rename column
-albums = albums.rename(columns={'Album': 'Album Name'})
-
-# Re-order columns
-albums = albums[['Album ID', 'Album Name']]
+# Drop duplicates; Add a primary key column; Rename; Re-order
+albums = process_dim_fields(
+    df=songs[['album']],
+    primary_key='album_id',
+    col_to_rename={'album': 'album_name'},
+    col_order=['album_id', 'album_name']
+)
 
 
 
-# Part 5: Song Performers=======================================================
+# # Part 5: Song Performers=======================================================
 song_performers = process_fact_fields(
-    df=songs[['Performed by']],
-    primary_key='Song Performer ID',
-    split_col_name='Performer Name',
-    col_to_split='Performed By',
+    df=songs[['performed_by']],
+    primary_key='song_performer_id',
+    col_to_rename={'performed_by': 'performers'},
+    split_col_name='performer_name',
+    col_to_split='performers',
     delimeter=', '
 )
 
-# Re-order columns
-song_performers = song_performers[['Song Performer ID', 'Performed By']]
 
 
-
-# Part 6: Performers ===========================================================
-performers = song_performers
-
+# # Part 6: Performers ===========================================================
 # Drop duplicates; Add a primary key column
-performers = process_dim_fields(performers[['Performer Name']], 'Performer ID')
-
-# Re-order columns
-performers = performers[['Performer ID', 'Performer Name']]
+performers = process_dim_fields(
+    df=song_performers[['performer_name']], 
+    primary_key='performer_id',
+    col_order=['performer_id', 'performer_name']
+)
 
 
 
 # Part 7: Create foreign keys ==================================================
 # Merge dataframes
-songs = pd.merge(songs, covers, left_on='Version covered', right_on='Version Covered Name')
-songs = pd.merge(songs, albums, left_on='Album', right_on='Album Name')
-songs = pd.merge(songs, song_performers, left_on='Performed by', right_on='Performed By')
-songs = songs.drop_duplicates(subset=['Song ID']).reset_index(drop=True)
-song_performers = pd.merge(song_performers, performers, on='Performer Name')
+songs = pd.merge(songs, covers, left_on='version_covered', right_on='cover_name')
+songs = pd.merge(songs, albums, left_on='album', right_on='album_name')
+songs = pd.merge(songs, song_performers, left_on='performed_by', right_on='performers')
+songs = songs.drop_duplicates(subset=['song_id']).reset_index(drop=True)
+song_performers = pd.merge(song_performers, performers, on='performer_name')
 
 # Keep only required columns
 songs = songs[[
-    'Song ID', 'Episode Code', 'Song Title', 'Cover ID', 'Song Performer ID', 
-    'Is Single', 'Album ID'
+    'song_id', 'episode_code', 'song_title', 'cover_id', 
+    'song_performer_id', 'album_id'
 ]]
-song_performers = song_performers[['Song Performer ID', 'Performer ID']]
+song_performers = song_performers[[
+    'song_performer_id', 'performer_id'
+]]
 
 
 
